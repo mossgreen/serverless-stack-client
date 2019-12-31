f398b5d6b5467a74bf3dc375c1ffd748b7e1f956
Moss GU <gufeifei@outlook.com>
2020-01-01 12:07:12 +1300

    client set up

---
+++ src/Routes.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import Home from "./containers/Home";
+import NotFound from "./containers/NotFound";
+import Login from "./containers/Login";
+import AppliedRoute from "./components/AppliedRoute";
+import Signup from "./containers/Signup";
+import NewNote from "./containers/NewNote";
+import Notes from "./containers/Notes";
+
+export default function Routes({ appProps }) {
+  return (
+    <Switch>
+      <AppliedRoute path="/" exact component={Home} appProps={appProps} />
+      <AppliedRoute path="/signup" exact component={Signup} appProps={appProps} />
+      <AppliedRoute path="/login" exact component={Login} appProps={appProps} />
+      <AppliedRoute path="/notes/new" exact component={NewNote} appProps={appProps} />
+      <AppliedRoute path="/notes/:id" exact component={Notes} appProps={appProps} />
+      { /* Finally, catch all unmatched routes */ }
+      <Route component={NotFound} />
+    </Switch>
+  );
+}
---
+++ src/config.js
@@ -0,0 +1,17 @@
+export default {
+  MAX_ATTACHMENT_SIZE: 5000000,
+    s3: {
+      REGION: "ap-southeast-2",
+      BUCKET: "serverless-notes-app-uploads-moss"
+    },
+    apiGateway: {
+      REGION: "ap-southeast-2",
+      URL: "https://jts417vbpb.execute-api.ap-southeast-2.amazonaws.com/prod"
+    },
+    cognito: {
+      REGION: "ap-southeast-2",
+      USER_POOL_ID: "ap-southeast-2_eLKi0GgOv",
+      APP_CLIENT_ID: "6t72itbvv0v6ikucvbn17d23g4",
+      IDENTITY_POOL_ID: "ap-southeast-2:b49aa4e1-3676-4133-9d7b-31e6d67de427"
+    }
+  };
